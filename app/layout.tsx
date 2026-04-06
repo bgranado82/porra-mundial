a14d4e7836d8cb84d5c4d21a7fb90ae90c484de6
Borja Granado <y4nsx52427@privaterelay.appleid.com>
2026-04-06 21:22:39 +0200

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,12 @@ export default function RootLayout({
       <head>
         <link rel="apple-touch-icon" href="/logo-192.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="Ibe World Cup" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
         />
+        <meta name="apple-mobile-web-app-title" content="Ibe World Cup" />
+        <meta name="application-name" content="Ibe World Cup" />
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body>{children}</body>
