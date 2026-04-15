a810b71811ca9bfd3344547cffb50e683d64f090
Borja Granado <y4nsx52427@privaterelay.appleid.com>
2026-04-15 08:10:13 +0200

    stats

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,6 +1,10 @@
-
+import { Suspense } from "react";
 import StatsPageClient from "@/components/StatsPageClient";
 
 export default function StatsPage() {
-  return <StatsPageClient />;
+  return (
+    <Suspense fallback={<div className="p-6">Cargando estadísticas...</div>}>
+      <StatsPageClient />
+    </Suspense>
+  );
 }
